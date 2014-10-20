bd4dbf91f4ec64b500a5410740e913dc3166e162
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-19 19:40:28 -0700

    Generated documentation for 905b431dbd52bfc31391187528e35aeface36c71

--- assets/behavior.js
+++ assets/behavior.js
@@ -140,6 +140,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
+            "title": "1.1.0 (2014-10-17)",
+            "slug": "110-2014-10-17"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
             "title": "1.0.0 (2014-10-02)",
             "slug": "100-2014-10-02"
           },
